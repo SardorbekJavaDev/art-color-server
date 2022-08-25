248187395290f9c96500991d900b917a5e1e3ef7
SardorbekJavaDev <sardorbekyorqulov1@gmail.com>
2022-08-25 22:29:38 +0500

    Initial commit

---
+++ src/main/java/com/company/artcolorserver/ArtColorServerApplication.java
@@ -0,0 +1,13 @@
+package com.company.artcolorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtColorServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtColorServerApplication.class, args);
+	}
+
+}
