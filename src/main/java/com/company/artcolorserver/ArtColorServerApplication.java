346a35dfd4cab95c570a4f3818ebf21c58432c6c
SardorbekJavaDev <sardorbekyorqulov1@gmail.com>
2022-08-25 22:42:19 +0500

    I am fixed the DB connection and application.properties

--- src/main/java/com/company/artcolorserver/ArtColorServerApplication.java
+++ src/main/java/com/company/artcolorserver/ArtColorServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ArtColorServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ArtColorServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ArtColorServerApplication.class, args);
+    }
 
 }
